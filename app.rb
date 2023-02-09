f63a2f5b5b067f547f3448aece65a0914399777a
Nabeel Akbar <studiosnabeel@gmail.com>
2023-02-09 20:31:50 +0500

    linters corrected

--- app.rb
+++ app.rb
@@ -12,11 +12,11 @@ class App
       '4' => 'Create a book',
       '5' => 'Create a rental',
       '6' => 'List all rentals for a given person   id',
-      '7'=> 'Exit'
-        } 
+      '7' => 'Exit'
+    }
 
     @books = []
-    
+
     @people = []
   end
 
@@ -61,13 +61,13 @@ class App
 
     print 'Has parent permission? [Y/N]: '
     permission = gets.chomp.downcase
-    
+
     case permission
     when 'y'
-      then parent_permission = true
+      parent_permission = true
     when 'n'
-      then parent_permission = false
-    end 
+      parent_permission = false
+    end
 
     @people.push(Student.new('11', age, name, parent_permission: parent_permission))
   end
@@ -91,9 +91,9 @@ class App
 
     case person_type
     when '1'
-      then create_student
+      create_student
     when '2'
-      then create_teacher
+      create_teacher
     end
 
     puts 'Person created successfully'
@@ -154,17 +154,17 @@ class App
   def choose_option(input)
     case input
     when '1'
-      then list_all_books
+      list_all_books
     when '2'
-      then list_all_people
+      list_all_people
     when '3'
-      then create_person
+      create_person
     when '4'
-      then create_book
+      create_book
     when '5'
-      then create_rental
+      create_rental
     when '6'
-      then rentals_of_person
+      rentals_of_person
     end
   end
 
